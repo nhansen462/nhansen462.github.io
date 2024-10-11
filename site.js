21493ff35528147fd2e3aad882e805b1baab0896
Nicholas Hansen <700259247@fvtc.edu>
2024-10-11 03:23:20 -0500

    Add files via upload
    Added Carousel (my gitbash on my home PC hates me and I don't want to deal with it right now)

--- site.js
+++ site.js
@@ -1,3 +1,4 @@
+// Welcome Message Assignment
 const hours = new Date().getHours();
 
 const isMorning = hours >= 4 && hours < 12; //morning check
@@ -16,4 +17,47 @@ if(isMorning){
     welcomeMessage.textContent = 'Good Day!';
 }
 
+// Secret Message Assignment
 localStorage.setItem('It\'s a secret to everybody.', 'There are as many truths to the world as there are people to tell them. Find the truth that speaks to you.');
+
+
+// Carousel Assignment
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+function advanceImages(){
+    showImages()
+    currentImage++;
+    if(currentImage === 5){currentImage = 0}
+}
+
+function reverseImages(){
+    showImages()
+    currentImage--;
+    if(currentImage === 0){currentImage = 4}
+}
+
+showImages()
+setInterval(() => advanceImages(), 5000)
+
+next.addEventListener('click', () => advanceImages())
+prev.addEventListener('click', () => reverseImages())
