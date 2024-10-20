16062a1eaa2788c55aa5672d66f5686f73277df6
Nicholas Hansen <700259247@fvtc.edu>
2024-10-19 23:36:53 -0500

    Finished Todolist

--- todo/todo.js
+++ todo/todo.js
@@ -8,7 +8,7 @@ localStorage.setItem('todo-list', JSON.stringify([
 
 const addTodoButton = document.querySelector('#add-item')
 const input = document.querySelector('#new-todo')
-const todoList = document.querySelector('#todo-list')
+const todoList = document.querySelector('.todo-list')
 
 // Get the list from local storage
 const todos = JSON.parse(localStorage.getItem('todo-list')) || []
@@ -18,22 +18,22 @@ const addTodo = () => {
     todos.push({ text: input.value, completed: false })
     // Save the list to local storage
     localStorage.setItem('todo-list', JSON.stringify(todos))
+    renderTodos()
+    input.value = ""
 }
+
 addTodoButton.addEventListener('click', () => addTodo())
 
-
-// Clear the li's before we recreate them
-//todoList.innerHTML = ''
-
-const test = document.querySelector('p')
-test.textContent = JSON.stringify(todos)
-
 const renderTodos = () => {
+    // Clear the li's before we recreate them
+    todoList.innerHTML = ''
     todos.forEach(todo =>{
         // Create and add new list items to the DOM
         const li = document.createElement('li')
         li.textContent = todo.text
         todoList.append(li)
+        
     })
 }
 
+renderTodos()
